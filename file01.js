b9fc2b0414eb867325c5a2aa9a557c6fb217185c
Manav Kumar <manav1811kumar@gmail.com>
2019-07-02 01:00:02 +0530

    Another random change

--- file01.js
+++ file01.js
@@ -1,8 +1,2 @@
-function greet(name)
-{
-    console.log('Hello '  + name)
-}
-
-greet('manav');
-
-console.log("Hey! Guys what's up");
+console.log('Hey! what is going on')
+console.log('Yeah everything is cool')
