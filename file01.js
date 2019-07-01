c17430b0acf5249caad4af1258266e8132348df4
Manav Kumar <manav1811kumar@gmail.com>
2019-07-02 01:11:42 +0530

    Now u can see more change

--- file01.js
+++ file01.js
@@ -1,2 +1,3 @@
 console.log('Hey! what is going on')
 console.log('Yeah everything is cool')
+console.log('Yeah one more line')
--- file02.js
+++ file02.js
@@ -3,3 +3,4 @@ console.log('Use git status to check status')
 console.log('Use git log to see history')
 
 console.log('How are u')
+console.log('Here also one more line')
