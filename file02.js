e561088ae1a273e24213f37ff8a14421a1a0b566
Manav Kumar <manav1811kumar@gmail.com>
2020-03-14 16:20:57 +0530

    Changes to js file

--- file02.js
+++ file02.js
@@ -1,6 +1,7 @@
 console.log("Hey! We'r learning git")
 console.log('Use "git status" to check status')
 console.log('Use "git log" to see history')
+console.log("Using js file in cpp folder")
 
 function newfun(name)
 {
